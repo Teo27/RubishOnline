0398cb7a0691faaf981d9685cf3d988ea856cf26
Goofy <karolisx4@gmail.com>
2016-11-16 13:14:07 +0200

    Added Views/Home

--- Model/Admin.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: Teo
- * Date: 16-Nov-16
- * Time: 11:47
- */
-
-
---
+++ Views/Home.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Goofy
+ * Date: 11/16/2016
+ * Time: 1:11 PM
+ */
